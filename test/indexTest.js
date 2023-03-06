4e87d92b53390e77d51a229ecd4ec6b5071c8c50
daniel <danielgonzalez252@gmail.com>
2023-03-06 03:59:24 -0500

    Lab Finished

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,45 @@
 require ( './root.js' );
 
+function shout(string)
+{
+  return string.toUpperCase();
+}
+
+function whisper(string)
+{
+  return string.toLowerCase();
+}
+
+function logShout(string)
+{
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string)
+{
+  if(string == string.toUpperCase())
+  {
+    return string = "YES INDEED!";
+
+  }
+
+  if(string == string.toLowerCase())
+  {
+    return string = "I can't hear you!";
+  }
+
+  if(string =="Let's have dinner together!" )
+  {
+    return string = "I would love to!";
+  }
+}
+
+
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
